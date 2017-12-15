e94b9a82fd7bf4ddaa726d7947a48f9f0ca09c1c
Pavan Hegde <pavanrkadave@gmail.com>
2017-12-15 16:02:08 +0530

    Added Register Functionality to the App using Firebase.

--- app/src/main/java/io/github/pavanrkadave/chatup/StartActivity.java
+++ app/src/main/java/io/github/pavanrkadave/chatup/StartActivity.java
@@ -1,13 +1,28 @@
 package io.github.pavanrkadave.chatup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button mRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        mRegBtn = (Button) findViewById(R.id.start_reg_button);
+
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(reg_intent);
+            }
+        });
     }
 }
