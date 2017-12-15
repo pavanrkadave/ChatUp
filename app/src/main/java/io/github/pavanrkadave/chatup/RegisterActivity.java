6ea72869b1753f12c2a8817d71284aae3f4f9eb4
Pavan Hegde <pavanrkadave@gmail.com>
2017-12-15 22:53:51 +0530

    Improved the Register user UI.added home as up indicator.added app bar, menu file.

--- app/src/main/java/io/github/pavanrkadave/chatup/RegisterActivity.java
+++ app/src/main/java/io/github/pavanrkadave/chatup/RegisterActivity.java
@@ -3,10 +3,12 @@ package io.github.pavanrkadave.chatup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,10 +18,12 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private TextInputLayout mDisplayName;
-    private TextInputLayout mEmail;
-    private TextInputLayout mPassword;
+    private EditText mDisplayName;
+    private EditText mEmail;
+    private EditText mPassword;
     private Button mCreateBtn;
+
+    private Toolbar mToolbar;
 
     private FirebaseAuth mAuth;
 
@@ -31,17 +35,23 @@ public class RegisterActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        mDisplayName = (TextInputLayout) findViewById(R.id.reg_display_name);
-        mEmail = (TextInputLayout) findViewById(R.id.reg_email);
-        mPassword = (TextInputLayout) findViewById(R.id.reg_password);
+        mDisplayName = (EditText) findViewById(R.id.reg_display_name);
+        mEmail = (EditText) findViewById(R.id.reg_email);
+        mPassword = (EditText) findViewById(R.id.reg_password);
         mCreateBtn = (Button) findViewById(R.id.reg_create_button);
+
+        mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Create Account");
+
 
         mCreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String display_name = mDisplayName.getEditText().toString();
-                String email = mEmail.getEditText().toString();
-                String password = mPassword.getEditText().toString();
+                String display_name = mDisplayName.getText().toString();
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+                Log.v("Credentials : ", " " + display_name + " " + email + " " + password);
 
                 register_user(display_name, email, password);
 
@@ -56,12 +66,12 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-
                     Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
                     startActivity(mainIntent);
                     finish();
                 } else {
                     Toast.makeText(RegisterActivity.this, "Authentication Failed!", Toast.LENGTH_SHORT).show();
+                    Log.e("Error : ", " " + task.getException());
                 }
             }
         });
