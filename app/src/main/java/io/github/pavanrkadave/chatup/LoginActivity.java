20a0a7f1261c86ce14e41525623e2fc5414f851a
Pavan Hegde <pavanrkadave@gmail.com>
2017-12-17 22:36:43 +0530

    Added Comments to LoginActivity

--- app/src/main/java/io/github/pavanrkadave/chatup/LoginActivity.java
+++ app/src/main/java/io/github/pavanrkadave/chatup/LoginActivity.java
@@ -41,17 +41,21 @@ public class LoginActivity extends AppCompatActivity {
         //Instance of firebase auth.
         mAuth = FirebaseAuth.getInstance();
 
+        //ToolBar For the Login Activity
         mLoginToolbar = (Toolbar) findViewById(R.id.login_toolbar);
         setSupportActionBar(mLoginToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Login");
 
+        //Setting Up the layout
         mLoginEmail = (EditText) findViewById(R.id.login_email);
         mLoginPassword = (EditText) findViewById(R.id.login_password);
         mLoginButton = (Button) findViewById(R.id.login_button);
 
+        //Progress Dialog
         mLoginProgress = new ProgressDialog(this);
 
+        //ClickListeners
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,16 +64,20 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (!TextUtils.isEmpty(login_email) || !TextUtils.isEmpty(login_password)) {
 
+                    //Showing the progress dialog when logging in
                     mLoginProgress.setTitle("Loging In..");
                     mLoginProgress.setMessage("Please Wait While We Check Your Credentials!");
                     mLoginProgress.setCanceledOnTouchOutside(false);
                     mLoginProgress.show();
+
+                    //Logging in user using firebase login with email and password
                     login_user(login_email, login_password);
                 }
             }
         });
     }
 
+    //method to log in user using email and password
     private void login_user(String login_email, String login_password) {
 
         mAuth.signInWithEmailAndPassword(login_email, login_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -78,17 +86,20 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (task.isSuccessful()) {
 
+                    //If the login is successful dismiss the progress dialog.
                     mLoginProgress.dismiss();
+                    //Send the intent to mainActivity if the user is successfully logged in.
                     Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
                     mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(mainIntent);
+                    //Kill the login activity after successful login.
                     finish();
                 } else {
+                    //If the login fails then hide the progress dialog with a toast message.
                     mLoginProgress.hide();
                     Toast.makeText(LoginActivity.this, "Error Logging In.. " + String.valueOf(task),
                             Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
 
