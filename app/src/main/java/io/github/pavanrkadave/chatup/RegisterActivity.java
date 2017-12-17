3d9234505299889ab11ebb67754d737b88297f03
Pavan Hegde <pavanrkadave@gmail.com>
2017-12-17 11:50:28 +0530

    => Added Firebase database to the app in register activity to store user information.

--- app/src/main/java/io/github/pavanrkadave/chatup/RegisterActivity.java
+++ app/src/main/java/io/github/pavanrkadave/chatup/RegisterActivity.java
@@ -17,6 +17,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,6 +31,9 @@ public class RegisterActivity extends AppCompatActivity {
     private Button mCreateBtn;
 
     private Toolbar mToolbar;
+
+    //Firebase Database Instance
+    private DatabaseReference mDatabase;
 
     //Progress bar
     private ProgressDialog mRegProgress;
@@ -71,12 +79,26 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void register_user(String display_name, String email, String password) {
+    private void register_user(final String display_name, String email, String password) {
 
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
+
+                    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                    String uid = current_user.getUid();
+
+                    mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+                    HashMap<String, String> userMap = new HashMap<>();
+                    userMap.put("name", display_name);
+                    userMap.put("status", "Hi there, I'm Using ChatUp!");
+                    userMap.put("image", "default");
+                    userMap.put("thumb_image", "default");
+
+                    mDatabase.setValue(userMap);
+
                     mRegProgress.dismiss();
                     Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
                     mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
